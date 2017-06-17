3876a24b09e59d8c95e264cdb2edbd364f4fa0da
Baltazar Ochagavia <bochagavia@uc.cl>
2017-06-17 01:19:52 -0400

    Add Basic Tests
    Test:
    - POST address
    - GET all addresses
    - GET address by valid id
    - GET address by invalid id
    
    Run tests by executing this command:
    $ rails test test/models/address_test.rb
    
    Answer:
    Finished in 0.665071s, 6.0144 runs/s, 13.5324 assertions/s.
    4 runs, 9 assertions, 0 failures, 0 errors, 0 skips

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -12,9 +12,55 @@
 #
 
 require 'test_helper'
+require 'net/http'
 
 class AddressTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @BASE_URI = "http://localhost:3000/api/addresses/"
+  end
+  
+
+  test 'POST address' do
+    uri = URI(@BASE_URI)
+    res = Net::HTTP.post_form(uri, string: 'alguna calle')
+    res_obj = JSON.parse(res.body)
+    assert_equal(res_obj["status"],"OK")
+    valid_id = res_obj["address"]["id"].to_i > 0
+    assert_equal(valid_id,true)
+  end
+
+  test 'GET all addresses' do
+    uri = URI(@BASE_URI)
+    res = Net::HTTP.get_response(uri)
+    res_obj = JSON.parse(res.body)
+    assert_equal(res_obj["status"],"OK")
+    valid_count = res_obj["addresses"].count > 0
+    assert_equal(valid_count,true)
+  end
+
+  test 'GET address by valid id' do
+    uri = URI(@BASE_URI+"6")
+    res = Net::HTTP.get_response(uri)
+    res_obj = JSON.parse(res.body)
+    assert_equal(res_obj["status"],"OK")
+    valid_latitude = res_obj["address"]["latitude"].to_i != 0
+    valid_longitude = res_obj["address"]["longitude"].to_i != 0
+    valid_elevation = res_obj["address"]["elevation"].to_i != 0
+    assert_equal(valid_latitude,true)
+    assert_equal(valid_longitude,true)
+    assert_equal(valid_elevation,true)
+  end
+
+  test 'GET address by invalid id' do
+    uri = URI(@BASE_URI)
+    res = Net::HTTP.get_response(uri+"-1")
+    res_obj = JSON.parse(res.body)
+    assert_equal(res_obj["status"],"INVALID_REQUEST")
+  end
+
+
 end
